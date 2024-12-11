e1fa2fe45fe2299462a4013273fd7b684a12911a
Adam <1173577350@qq.com>
2024-12-11 12:55:56 +0800

    写更新项目前状态12.11

--- WpfApp1/ViewModels/MainViewModels.cs
+++ WpfApp1/ViewModels/MainViewModels.cs
@@ -45,72 +45,7 @@ namespace WpfApp1.ViewModels
         public ChartValues<double> Voltage { get; set; }
 
 
-        //private ChartValues<double> windSpeedValues;
-        //public ChartValues<double> WindSpeedValues
-        //{
-        //    get { return windSpeedValues; }
-        //    set
-        //    {
-        //        windSpeedValues = value;
-        //        SetPropertyChanged(nameof(windSpeedValues));
-        //    }
-
-        //}
-        //private ChartValues<double> adam;
-        //public ChartValues<double> Adam
-        //{
-        //    get { return adam; }
-        //    set
-        //    {
-        //        adam = value;
-        //        SetPropertyChanged(nameof(adam));
-        //    }
-
-        //}
-        //private ChartValues<double> adam1;
-        //public ChartValues<double> Adam1
-        //{
-        //    get { return adam1; }
-        //    set
-        //    {
-        //        adam = value;
-        //        SetPropertyChanged(nameof(adam1));
-        //    }
-
-        //}
-        //private ChartValues<double> temperaturValues;
-        //public ChartValues<double> TemperaturValues
-        //{
-        //    get { return temperaturValues; }
-        //    set
-        //    {
-        //        windSpeedValues = value;
-        //        SetPropertyChanged(nameof(temperaturValues));
-        //    }
-
-        //}
-        //private ChartValues<double> humidityValues;
-        //public ChartValues<double> HumidityValues
-        //{
-        //    get { return humidityValues; }
-        //    set
-        //    {
-        //        windSpeedValues = value;
-        //        SetPropertyChanged(nameof(humidityValues));
-        //    }
-
-        //}
-        //private ChartValues<double> windPowerDensity;
-        //public ChartValues<double> WindPowerDensity
-        //{
-        //    get { return windPowerDensity; }
-        //    set
-        //    {
-        //        windSpeedValues = value;
-        //        SetPropertyChanged(nameof(windPowerDensity));
-        //    }
-
-        //}
+    
 
 
         public SelectDate StartDate { get; set; }     
@@ -124,41 +59,7 @@ namespace WpfApp1.ViewModels
             get { return saleBillboards; }
             set { saleBillboards = value; }
         }
-        //private ObservableCollection<WindSpeedData> ReadWindSpeedDataFromCsv(string filePath)
-        //{
-        //    var data = new ObservableCollection<WindSpeedData>();
-
-        //    using (var reader = new StreamReader(filePath))
-        //    {
-        //        var line = reader.ReadLine();  // 跳过表头
-
-        //        while ((line = reader.ReadLine()) != null)
-        //        {
-        //            var values = line.Split(',');
-        //            var timeRange = values[0];  // 假设 values[0] = "2024-01-03 - 2024-01-05"
-        //            var startDateString = timeRange.Split(' ')[0];  // 提取日期 "2024-01-03"
-
-        //            var windSpeed = double.Parse(values[1]);
-        //            var temperature = double.Parse(values[2]);
-        //            var humidity = double.Parse(values[3]);
-        //            var windPowerDensity = double.Parse(values[4]);
-
-
-        //            data.Add(new WindSpeedData
-        //            {
-        //                WindSpeed = windSpeed
-        //                ,
-        //                Temperature = temperature
-        //                ,
-        //                Humidity = humidity,
-        //                WindPowerDensity = windPowerDensity
-        //            });
-        //        }
-        //    }
-
-        //    return data;
-        //}
-
+     
         private ObservableCollection<WindSpeedData> ReadWindSpeedDataFromCsv2(string filePath)
         {
             var data = new ObservableCollection<WindSpeedData>();
@@ -286,22 +187,24 @@ namespace WpfApp1.ViewModels
         {
             EndDate = new SelectDate();
             StartDate = new SelectDate();
-
+            string datacsv = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "weather_data_with_wpd.csv");
+            string dataZf = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "frequency_data.csv");
+            string datascada = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "processed_output_12_bins.csv");
 
             // 读取 CSV 文件并转换成 WindSpeedData 对象列表
-            var data = ReadWindSpeedDataFromCsv("E:\\gird\\weather_data_with_wpd.csv");
+            var data = ReadWindSpeedDataFromCsv(datacsv);
 
             // 转化为 LiveCharts 图表数据
             WindSpeedValues = new ChartValues<double>(data.Select(d => d.WindSpeed));
             TemperaturValues = new ChartValues<double>(data.Select(d => d.Temperature));
             HumidityValues = new ChartValues<double>(data.Select(d => d.Humidity));
             WindPowerDensity = new ChartValues<double>(data.Select(d => d.WindPowerDensity));
-            var dataZF = ReadWindSpeedDataFromCsv1("E:\\gird\\frequency_data.csv");
+            var dataZF = ReadWindSpeedDataFromCsv1(dataZf);
             WindSpeedValuesZF = new ChartValues<double>(dataZF.Select(d => d.WindSpeed));
             TemperaturValuesZF = new ChartValues<double>(dataZF.Select(d => d.Temperature));
             HumidityValuesZF = new ChartValues<double>(dataZF.Select(d => d.Humidity));
             WindPowerDensityZF = new ChartValues<double>(dataZF.Select(d => d.WindPowerDensity));
-            var dataScada = ReadWindSpeedDataFromCsvScada("E:\\gird\\processed_output_12_bins.csv");
+            var dataScada = ReadWindSpeedDataFromCsvScada(datascada);
             WindSpeed =new ChartValues<double> (dataScada.Select(d => d.WindSpeed));
             Power = new ChartValues<double>(dataScada.Select(d => d.Power));
             Vane =new ChartValues<double>(dataScada.Select ((d) => d.Vane));
